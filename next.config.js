cb8bcc8729ee68d6641aaccbfb7cffc340f77a1b
flamrdevs <flamrdevs@gmail.com>
2022-10-14 19:30:55 +0700

    Optional split chunks

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({ enabled: process.env.ANALYZE === "true", openAnalyzer: true });
 const TerserPlugin = require("terser-webpack-plugin");
 
+function itemFn(item) {
+  return item;
+}
+function moduleToFilename(module) {
+  return module.identifier().split("/").reduceRight(itemFn);
+}
+
+function createTest(filename) {
+  return (regexp) => {
+    return new RegExp(regexp).test(filename);
+  };
+}
+
+function createBase(name) {
+  return function (...ps) {
+    return [name].concat(ps).join(".");
+  };
+}
+
 /**
  * @type {import('next').NextConfig}
  **/
@@ -17,7 +36,7 @@ const nextConfig = {
   /**
    * Opt : Custom webpack
    */
-  webpack: (config, {}) => {
+  webpack: (config, { isServer }) => {
     /**
      * Opt : Unminified bundle
      */
@@ -37,61 +56,81 @@ const nextConfig = {
      * Opt : Split chunks
      */
 
-    const itemFn = (item) => item;
-    const moduleToFilename = (module) => module.identifier().split("/").reduceRight(itemFn);
+    const enable = false;
+    if (enable) {
+      config.optimization.splitChunks.cacheGroups[`io-react`] = {
+        test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+        name: "react",
+        chunks: "all",
+      };
 
-    const cacheGroup = (test, name) => ({ test, name, chunks: "all" });
-
-    config.optimization.splitChunks.cacheGroups = {
-      ["react"]: cacheGroup(/[\\/]node_modules[\\/](react|react-dom)[\\/]/, "react"),
-
-      ["ui"]: {
+      config.optimization.splitChunks.cacheGroups[`io-ui`] = {
         test: /[\\/]node_modules[\\/](@emotion|@mantine)[\\/]/,
         name(module) {
-          const name = "ui";
           const filename = moduleToFilename(module);
-          const test = (regexp) => new RegExp(regexp).test(filename);
-          const base = (...ps) => [name].concat(ps).join("/");
-          if (test(/[\\/]@mantine[\\/]carousel[\\/]/)) return base("carousel");
-          if (test(/[\\/]@mantine[\\/]dates[\\/]/)) return base("dates");
-          if (test(/[\\/]@mantine[\\/]dropzone[\\/]/)) return base("dropzone");
-          if (test(/[\\/]@mantine[\\/]form[\\/]/)) return base("form");
-          if (test(/[\\/]@mantine[\\/]modals[\\/]/)) return base("modals");
-          if (test(/[\\/]@mantine[\\/]notifications[\\/]/)) return base("notifications");
-          if (test(/[\\/]@mantine[\\/]prism[\\/]/)) return base("prism");
-          if (test(/[\\/]@mantine[\\/]rte[\\/]/)) return base("rte");
-          if (test(/[\\/]@mantine[\\/]spotlight[\\/]/)) return base("spotlight");
-          return base();
+          const test = createTest(filename);
+
+          const ui_base = createBase("ui");
+          if (test(/[\\/]@mantine[\\/]carousel[\\/]/)) return ui_base("carousel");
+          if (test(/[\\/]@mantine[\\/]dates[\\/]/)) return ui_base("dates");
+          if (test(/[\\/]@mantine[\\/]dropzone[\\/]/)) return ui_base("dropzone");
+          if (test(/[\\/]@mantine[\\/]form[\\/]/)) return ui_base("form");
+          if (test(/[\\/]@mantine[\\/]modals[\\/]/)) return ui_base("modals");
+          if (test(/[\\/]@mantine[\\/]notifications[\\/]/)) return ui_base("notifications");
+          if (test(/[\\/]@mantine[\\/]prism[\\/]/)) return ui_base("prism");
+          if (test(/[\\/]@mantine[\\/]rte[\\/]/)) return ui_base("rte");
+          if (test(/[\\/]@mantine[\\/]spotlight[\\/]/)) return ui_base("spotlight");
+          return ui_base();
         },
         chunks: "all",
-      },
+      };
 
-      ["icon"]: cacheGroup(/[\\/]node_modules[\\/]@tabler[\\/]/, "icon"),
-      ["jsonforms"]: cacheGroup(/[\\/]node_modules[\\/]@jsonforms[\\/]/, "jsonforms"),
-      ["dnd-kit"]: cacheGroup(/[\\/]node_modules[\\/]@dnd-kit[\\/]/, "dnd-kit"),
+      config.optimization.splitChunks.cacheGroups[`io-icon`] = {
+        test: /[\\/]node_modules[\\/]@tabler[\\/]/,
+        name: "icon",
+        chunks: "all",
+      };
+      config.optimization.splitChunks.cacheGroups[`io-jsonforms`] = {
+        test: /[\\/]node_modules[\\/]@jsonforms[\\/]/,
+        name: "jsonforms",
+        chunks: "all",
+      };
+      config.optimization.splitChunks.cacheGroups[`io-dnd-kit`] = {
+        test: /[\\/]node_modules[\\/]@dnd-kit[\\/]/,
+        name: "dnd-kit",
+        chunks: "all",
+      };
 
-      ["firebase"]: {
+      config.optimization.splitChunks.cacheGroups[`io-firebase`] = {
         test: /[\\/]node_modules[\\/]@firebase[\\/]/,
         name(module) {
-          const name = "firebase";
           const filename = moduleToFilename(module);
-          const test = (regexp) => new RegExp(regexp).test(filename);
-          const base = (...ps) => [name].concat(ps).join("/");
-          if (test(/[\\/]@firebase[\\/]app[\\/]/)) return base("app");
-          if (test(/[\\/]@firebase[\\/]auth[\\/]/)) return base("auth");
-          if (test(/[\\/]@firebase[\\/]firestore[\\/]/)) return base("firestore");
-          if (test(/[\\/]@firebase[\\/]storage[\\/]/)) return base("storage");
-          if (test(/[\\/]@firebase[\\/]util[\\/]/)) return base("util");
-          if (test(/[\\/]@firebase[\\/]logger[\\/]/)) return base("logger");
-          if (test(/[\\/]@firebase[\\/]component[\\/]/)) return base("component");
-          return base();
+          const test = createTest(filename);
+
+          const firebase_base = createBase("firebase");
+          if (test(/[\\/]@firebase[\\/]app[\\/]/)) return firebase_base("app");
+          if (test(/[\\/]@firebase[\\/]auth[\\/]/)) return firebase_base("auth");
+          if (test(/[\\/]@firebase[\\/]firestore[\\/]/)) return firebase_base("firestore");
+          if (test(/[\\/]@firebase[\\/]storage[\\/]/)) return firebase_base("storage");
+          if (test(/[\\/]@firebase[\\/]util[\\/]/)) return firebase_base("util");
+          if (test(/[\\/]@firebase[\\/]logger[\\/]/)) return firebase_base("logger");
+          if (test(/[\\/]@firebase[\\/]component[\\/]/)) return firebase_base("component");
+          return firebase_base();
         },
         chunks: "all",
-      },
+      };
 
-      ["ky"]: cacheGroup(/[\\/]node_modules[\\/]ky[\\/]/, "ky"),
-      ["swr"]: cacheGroup(/[\\/]node_modules[\\/]swr[\\/]/, "swr"),
-    };
+      config.optimization.splitChunks.cacheGroups[`io-ky`] = {
+        test: /[\\/]node_modules[\\/]ky[\\/]/,
+        name: "ky",
+        chunks: "all",
+      };
+      config.optimization.splitChunks.cacheGroups[`io-swr`] = {
+        test: /[\\/]node_modules[\\/]swr[\\/]/,
+        name: "swr",
+        chunks: "all",
+      };
+    }
 
     return config;
   },
